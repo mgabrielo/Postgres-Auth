28e96ab860dacc7636684f6f6b47dd870e8ead8a
Scott Nitro <scottnitro1@gmail.com>
2024-03-06 21:15:04 +0000

    1st commit

--- backend/db.sql
+++ backend/db.sql
@@ -5,6 +5,3 @@ CREATE TABLE users(
   email VARCHAR(100) UNIQUE NOT NULL,
   password VARCHAR(255) NOT NULL
 );
-
-INSERT INTO users(username,email,password) VALUES ('grey','grey@email.com','password');
-
